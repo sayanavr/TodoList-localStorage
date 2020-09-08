cf3688f88ea502301694a46fad73f013e2d05a14
sayanavr <c0753753@mylambton.ca>
2020-09-08 17:47:11 -0400

    committed

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app';
+  todoArray=[];
+  addTodo(value){    
+    this.todoArray.push(value);  
+    }
+    deleteItem(todo){
+      for(let i=0 ;i<= this.todoArray.length ;i++){
+         if(todo== this.todoArray[i]){ 
+            this.todoArray.splice(i,1)    
+          }   
+        }  
+      }
+      keyDownFunction(event) {
+        if (event.keyCode === 13) {
+          alert('you just pressed the enter key');
+          // rest of your code
+          //this.addTodo(todo);
+          event.preventDefault();
+        }
+      }
+}
