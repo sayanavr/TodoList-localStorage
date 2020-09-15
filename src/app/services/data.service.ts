c3612e82f95abf5d323799a6399fd5cf58df555f
sayanavr <c0753753@mylambton.ca>
2020-09-15 14:05:07 -0400

    Added Local storage functionality

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -7,9 +7,16 @@ export class DataService {
   todoArray=[];
   constructor() { }
   getTodoList(){
+    if(localStorage.getItem('todoArray')===null){
+      this.todoArray=[];
+    }
+    else{
+      this.todoArray=JSON.parse(localStorage.getItem('todoArray'));
+    }
     return this.todoArray;
   }
   setTodoList(newtodoArr){
     this.todoArray=newtodoArr;
+    localStorage.setItem('todoArray',JSON.stringify(this.todoArray));
   }
 }
